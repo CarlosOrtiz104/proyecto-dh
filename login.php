eb382c0ec1a175de47475abaadbd09fa1d1b1550
Carlos <carlosortizj104@hotmail.com>
2018-03-24 16:37:34 -0400

    styleFr correponde a un modelo para preguntas frecuentes, preguntasFre es una mascara para rellenar conn el contenido, paquetesUtiles es la carpeta donde guarde el header como funcion y el footer que esta en proceso de desarrollo

--- login.php
+++ login.php
@@ -8,74 +8,60 @@
 	<link rel="stylesheet" href="font/fonts.css">
 	<script src="http://code.jquery.com/jquery-latest.js"></script>
 	<script src="js/main.js"></script>
-	
+
 <!--inicio cabecera login-->
   <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
   <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
   <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
   <!--fin cabecera login-->
 
+<!--Cabecera del footer-->
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!--Fin de cabecera footer-->
 </head>
 <body>
-  <header>
-		<div class="menu_bar">
-			<a href="#" class="bt-menu"><span class="icon-menu"></span>Menu</a>
-		</div>
+	<?php require("paquetesUtiles/header.php"); ?>
 
-		<nav>
-			<ul>
-				<li><a href="#"><span class="icon-home"></span>Inicio</a></li>
-				<li><a href="#"><span class="icon-pen"></span>Trabajos</a></li>
-				<li class="submenu">
-					<a href="#"><span class="icon-rocket"></span>Proyectos<span class="caret icon-arrow-down6"></span></a>
-					<ul class="children">
-						<li><a href="#">Dato sub menu<span class="icon-quill"></span></a></li>
-						<li><a href="#">Dato sub menu 2<span class="icon-quill"></span></a></li>
-						<li><a href="#">Dato sub menu 3<span class="icon-quill"></span></a></li>
-					</ul>
-				</li>
-				<li><a href="#"><span class="icon-tree"></span>Servicios</a></li>
-				<li><a href="#"><span class="icon-mobile"></span>Contacto</a></li>
-        <li><a href="#"><span class="icon-compass"></span>Login</a></li>
-			</ul>
-		</nav>
+		<div class="container">
+				<div class="row">
+						<div class="col-md-12">
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-
-                <div class="wrap">
-                    <p class="form-title">
+								<div class="wrap">
+										<p class="form-title">
 											<img src="imagen/redbrick.png" width="21%"alt=""><br>
-                     Bienvenido!!</p>
-                    <form class="login">
-                    <input type="text" placeholder="Username" pattern="^[\w._%-]+@[\w.-]+\.[a-zA-Z]{2,4}$" required/>
-                    <input type="password" placeholder="Password" required/>
-                    <input type="submit" value="Iniciar" class="btn btn-success btn-sm" />
-                    <a class="center-reset"><input type="reset" value="reset" class="btn btn-success btn-sm" /></a>
-                    <div class="remember-forgot">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" />
-                                        Recuerdame
-                                    </label>
-                                </div>
-                            </div>
-                            <div class="col-md-6 forgot-pass-content">
-                                <a href="#" class="forgot-pass">Olvide mi clave</a>
-                            </div>
-                        </div>
-                    </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+										 Bienvenido!!</p>
+										<form class="login">
+										<input type="text" placeholder="Username" pattern="^[\w._%-]+@[\w.-]+\.[a-zA-Z]{2,4}$" required/>
+										<input type="password" placeholder="Password" required/>
+										<input type="submit" value="Iniciar" class="btn btn-success btn-sm" />
+										<a class="center-reset"><input type="reset" value="reset" class="btn btn-success btn-sm" /></a>
 
-
-	</header>
+										<div class="remember-forgot">
+												<div class="row">
+														<div class="col-md-6">
+																<div class="checkbox">
+																		<label>
+																				<input type="checkbox" />
+																				Recuerdame
+																		</label>
+																</div>
+														</div>
+														<div class="col-md-6 forgot-pass-content">
+																<a href="#" class="forgot-pass">Olvide mi clave</a>
+														</div>
+														<div class="col-md-6 register">
+																<a href="registro.php" class="register">No esta registrado ?</a>
+														</div>
+												</div>
+										</div>
+										</form>
+								</div>
+						</div>
+				</div>
+		</div>
 
 </body>
 </html>
---
+++ paquetesUtiles/header.php
@@ -0,0 +1,24 @@
+<header>
+  <div class="menu_bar">
+    <a href="#" class="bt-menu"><span class="icon-menu"></span>Menu</a>
+  </div>
+
+  <nav>
+    <ul>
+      <li><a href="#"><span class="icon-home"></span>Inicio</a></li>
+      <li><a href="#"><span class="icon-pen"></span>Trabajos</a></li>
+      <li class="submenu">
+        <a href="#"><span class="icon-rocket"></span>Proyectos<span class="caret icon-arrow-down6"></span></a>
+        <ul class="children">
+          <li><a href="#">Dato sub menu<span class="icon-quill"></span></a></li>
+          <li><a href="#">Dato sub menu 2<span class="icon-quill"></span></a></li>
+          <li><a href="#">Dato sub menu 3<span class="icon-quill"></span></a></li>
+        </ul>
+      </li>
+      <li><a href="#"><span class="icon-tree"></span>Servicios</a></li>
+      <li><a href="#"><span class="icon-mobile"></span>Contacto</a></li>
+      <li><a href="#"><span class="icon-compass"></span>Login</a></li>
+    </ul>
+  </nav>
+
+</header>
