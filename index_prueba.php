ec74a617990cac1023bcc4789a5acaad6ce0a021
Cristian <schweizercristian@gmail.com>
2018-03-23 22:20:43 -0300

    subo prueba de index

---
+++ index_prueba.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="font/fonts.css">
+		<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  </head>
+  <body>
+    <div class="container">
+      <header class"header">
+
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </div>
+
+  </body>
+</html>
